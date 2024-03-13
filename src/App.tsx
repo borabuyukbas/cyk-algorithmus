3aac96926d4bdb31f7df57849da624fb75ba6341
Bora Büyükbaş <borabuyukbas@hotmail.com>
2024-03-14 01:08:10 +0300

    feat: implement basic Grammatik class and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
+import { Grammatik } from "./Grammatik.ts"
+
 function App() {
+  const [rules, setRules] = useState("");
+  const [word, setWord] = useState("");
+
   return (
     <>
-      <h1>CYK Algorithmus</h1>
+      <div className="flex flex-col lg:flex-row mb-4 items-center justify-between">
+        <h1 className="text-4xl font-bold flex-shrink-0 mb-2 lg:mb-0">CYK Algorithmus</h1>
+        <p>Cocke-Younger-Kasami-Algorithmus für Lösen des Wortproblems für kontextfreie Sprachen</p>
+      </div>
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col basis-1/4 mb-4 lg:mr-4 lg:mb-0">
+          <div className="mb-4 flex flex-col">
+            <label htmlFor="produktionsregeln">Produktionsregeln von Grammatik mit Chomsky-Normalform</label>
+            <textarea
+              id="produktionsregeln"
+              className="px-4 py-2 bg-neutral-200 dark:bg-neutral-700 rounded-lg resize-none h-48"
+              onChange={e => setRules(e.target.value)}
+              value={rules}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="wort">Wort</label>
+            <input
+              id="wort"
+              type="text"
+              className="px-4 py-2 bg-neutral-200 dark:bg-neutral-700 rounded-lg mb-4"
+              value={word}
+              onChange={e => setWord(e.target.value)}
+            />
+          </div>
+          <button
+            className="bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-700 dark:hover:bg-neutral-600 transition-colors px-4 py-2 rounded-lg"
+            onClick={ _ => console.log(Grammatik.fromChomskyNormalform(rules)) }
+          >
+            Rechnen
+          </button>
+        </div>
+        <div>
+          Syntaxbaum für {word} mit Regeln {rules}
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/Grammatik.ts
@@ -0,0 +1,75 @@
+// Grammatik in Chomsky-Normalform
+export class Grammatik {
+  variablen: Set<string>;
+  terminale: Set<string>;
+  startsymbol: string;
+  produktionRegeln: Map<string, Set<string>>;
+
+  public constructor(
+    variablen: Set<string>,
+    terminale: Set<string>,
+    startsymbol: string
+  ) {
+    if (!variablen.has(startsymbol)) throw Error('Startsymbol existiert nicht in `variablen`.');
+
+    this.variablen = variablen;
+    this.terminale = terminale;
+    this.startsymbol = startsymbol;
+    this.produktionRegeln = new Map();
+  }
+
+  public addRegel(variabel: string, produktion: string) {
+    if (this.pruefenRegeln(variabel, produktion)) {
+      if (!this.produktionRegeln.has(variabel)) this.produktionRegeln.set(variabel, new Set())
+      this.produktionRegeln.set(variabel, this.produktionRegeln.get(variabel)!.add(produktion))
+    }
+  }
+
+  public removeRegel(variabel: string, produktion: string) {
+    if (this.produktionRegeln.has(variabel))
+      this.produktionRegeln.get(variabel)!.delete(produktion)
+  }
+
+  public static fromChomskyNormalform(text: string): Grammatik {
+    // _ als Platzholder für neue Grammatik
+    let grammatik = new Grammatik(new Set(["_"]), new Set(), "_");
+    
+    let tempRegeln = new Map<string, Set<string>>();
+
+    for (const [index, zeile] of text.split("\n").entries()) {
+      let gefilterteZeile = zeile.replace(/\s/g, "");
+      let zeileParts = gefilterteZeile.split("->");
+      if (zeileParts.length == 2) {
+        let [variabel, regelnText] = gefilterteZeile.split("->");
+        let regeln = regelnText.split("|");
+  
+        grammatik.variablen.add(variabel);
+        if (index == 0) grammatik.startsymbol = variabel;
+
+        for (const regel of regeln) {
+          if (regel.length == 1 && regel != "ϵ") grammatik.terminale.add(regel);
+          tempRegeln.set(variabel, tempRegeln.has(variabel) ?
+           tempRegeln.get(variabel)!.add(regel == "ϵ" ? "" : regel) : new Set([regel == "ϵ" ? "" : regel]));
+        }
+      }
+    }
+
+    // Löschen von Platzholder-Variabel
+    grammatik.variablen.delete("_");
+
+    tempRegeln.forEach((value, key) => value.forEach(r => grammatik.addRegel(key, r)))
+    return grammatik;
+  }
+
+  private pruefenRegeln(variabel: string, produktion: string): boolean {
+    if (!this.variablen.has(variabel)) return false;
+    if (produktion.length > 2) return false;
+    if (produktion.length == 2
+      && (!this.variablen.has(produktion.charAt(0)) || !this.variablen.has(produktion.charAt(1))
+      || produktion.charAt(0) == this.startsymbol || produktion.charAt(1) == this.startsymbol)) return false;
+    if (produktion.length == 1 && !this.terminale.has(produktion)) return false;
+    if (produktion.length == 0 && variabel != this.startsymbol) return false;
+
+    return true;
+  }
+}
